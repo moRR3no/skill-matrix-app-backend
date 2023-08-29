bf15ddda8a44f01b38ead271ba6127ec168f1bb3
morreno <konradpstrongowski@gmail.com>
2023-08-29 14:18:10 +0200

    Tidy up gradle file

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,13 +22,13 @@ repositories {
 }
 
 dependencies {
-	implementation("org.mapstruct:mapstruct:1.4.2.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
+	implementation("org.mapstruct:mapstruct:1.4.2.Final")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.4.2.Final")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
